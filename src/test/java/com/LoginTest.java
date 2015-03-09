a908abf6b8a718c5a1220143a639ecc610590c0e
horatiugorgan <horatiugorgan@172.22.8.72>
2015-03-09 11:37:37 +0200

    initial project

---
+++ src/test/java/com/LoginTest.java
@@ -0,0 +1,44 @@
+package com;
+
+import net.thucydides.core.annotations.Issue;
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Pending;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.requirements.Application;
+import com.steps.LoginSteps;
+
+@Story(Application.Search.SearchByKeyword.class)
+@RunWith(ThucydidesRunner.class)
+public class LoginTest {
+
+    @Managed(uniqueSession = true)
+    public WebDriver webdriver;
+
+    @ManagedPages(defaultUrl = "http://192.168.1.68:9090/login")
+    public Pages pages;
+
+    @Steps
+    public LoginSteps endUser;
+
+    @Issue("#VACATION-1")
+
+    @Test 
+    public void login_action(){
+    	endUser.is_the_login_page();
+    	endUser.enter_username("malu.ioan1");
+    	endUser.enter_password("evoportal");
+    	endUser.login_click();
+    	
+    }
+    
+ 
+} 
---
+++ src/test/java/com/steps/LoginSteps.java
@@ -0,0 +1,41 @@
+package com.steps;
+
+import com.pages.LoginPage;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.steps.ScenarioSteps;
+import static ch.lambdaj.Lambda.join;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+
+public class LoginSteps extends ScenarioSteps {
+
+    LoginPage loginPage;
+
+    
+    @Step
+    public void enter_username(String username) {
+    	loginPage.enter_username(username);
+    }
+
+    
+    @Step
+    public void enter_password(String password) {
+    	loginPage.enter_password(password);
+    }
+    
+    @Step	
+    public void login_click(){
+    	loginPage.login_click();
+    	
+    }
+
+    @Step
+    public void is_the_login_page() {
+        loginPage.open();
+    }
+    
+
+}
